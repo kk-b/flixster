5c821d8be827eda13925e54ea40f74e47018782b
Kyle Khuong-Bui <kyle.bui20@gmail.com>
2021-01-20 22:38:17 -0800

    update

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Kyle Khuong-Bui on 1/20/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
